580efdf390eebc42b61bad66e9d627b739c1d10b
Ashiri <ajaoiyanu@gmail.com>
2019-03-04 23:17:47 +0100

    Add init command

--- commands.py
+++ commands.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import json
+
+from terminaltables import SingleTable
+import click
 
 
 folder = Path().home() / 'source_data'
@@ -10,3 +14,16 @@ directory = folder / 'commands.json'
 directory.touch()
 
 id_directory = folder / 'file.txt'
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command('init', short_help='Initialize the file')
+def init():
+    with open(directory, 'w') as f:
+        commands = dict()
+        json.dump(commands, f)
+    click.echo('File has been initialized')
